73233d00d26035d7202a8ea274ac19ddaed1b0b1
chen <2190826197@qq.com>
2022-06-09 14:04:24 +0800

    master冲突1

--- src/main/java/com/example/springbootdemo1/hello/Hello.java
+++ src/main/java/com/example/springbootdemo1/hello/Hello.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Hello {
     @RequestMapping("/hello1")
     public void hello(){
-        System.out.println("hello world");
+        System.out.println("hello");
     }
 
     @RequestMapping("/say")
