8a5b13204bc852397ee2dd1a9a39eba024549354
chen <2190826197@qq.com>
2022-06-09 13:54:46 +0800

    hot-fix合并测试1

--- src/main/java/com/example/springbootdemo1/hello/Hello.java
+++ src/main/java/com/example/springbootdemo1/hello/Hello.java
@@ -4,9 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/hello1")
 public class Hello {
+    @RequestMapping("/hello1")
     public void hello(){
         System.out.println("hello world");
+    }
+
+    @RequestMapping("/say")
+    public void say(){
+        System.out.println("say...");
     }
 }
